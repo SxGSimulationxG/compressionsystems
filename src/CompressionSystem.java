3524cb11dcb827537df28ffdd37538fca59ca180
SxGSimulationxG <kr.kairasmussen@outlook.com>
2025-10-05 23:01:18 +0200

    comments

--- src/CompressionSystem.java
+++ src/CompressionSystem.java
@@ -15,21 +15,17 @@ public abstract class CompressionSystem {
     public Object[] analyzeCharacters(char[] input) {
         Map<Character, Integer> frequencyMap = new HashMap<>();
 
-        // Count how often each character appears
         for (char c : input) {
             frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
         }
 
-        // Convert to a sortable list
         List<Map.Entry<Character, Integer>> entries = new ArrayList<>(frequencyMap.entrySet());
 
-        // Sort by frequency (descending), then alphabetically
         entries.sort((a, b) -> {
             int freqCompare = Integer.compare(b.getValue(), a.getValue());
             return (freqCompare != 0) ? freqCompare : Character.compare(a.getKey(), b.getKey());
         });
 
-        // Build result arrays
         char[] uniqueChars = new char[entries.size()];
         int[] frequencies = new int[entries.size()];
 
